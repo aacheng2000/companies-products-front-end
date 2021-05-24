c8b1e2465032a8071c7795fc14b9c639b170bb2d
EC2 Default User <ec2-user@ip-172-31-37-117.us-east-2.compute.internal>
2021-05-24 22:29:39 +0000

    got webpack to work

---
+++ src/index.js
@@ -0,0 +1,7 @@
+const faker = require('faker')
+console.log(faker.company.companyName());
+console.log(faker.commerce.productName());
+
+const companyList = document.querySelector('#company-list');
+console.log(companyList)
+
